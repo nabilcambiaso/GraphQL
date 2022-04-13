512e4d7f07e2a2512dc4be46d493915c32c5c5d0
nabilcambiaso <nabil.cambiaso@gmail.com>
2022-04-13 03:00:31 +0000

    Fully working GraphQL api with queries and mutations

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,27 +1,63 @@
-const {UserList,MovieList} = require("../fakeData");
+const { UserList, MovieList } = require("../fakeData");
 const _ = require("lodash");
+
+
 const resolvers = {
+
     Query: {
         //USERS RESOLVERS
         users: () => {
-          return UserList;
+            return UserList;
         },
-        user: (parent,args) => {
-           const id = args.id;
-           const user = _.find( UserList, {id:Number(id)})
-           return user;
+        user: (parent, args) => {
+            const id = args.id;
+            const user = _.find(UserList, { id: Number(id) })
+            return user;
         },
 
         //MOVIES RESOLVERS
         movies: () => {
             return MovieList;
         },
-        movie: (parent,args) => {
+        movie: (parent, args) => {
             const name = args.name;
-            const movie = _.find( MovieList, {name})
+            const movie = _.find(MovieList, { name })
             return movie;
+        }
+    },
+    Mutation:{
+        createUser: (parent,args)=>{
+            const user=args.input;
+            const id=Number(UserList[UserList.length-1].id)+1;
+            user.id= id;
+            UserList.push(user);
+            return user;
+        },
+        updateUserName: (parent,args) => {
+            const {id,updatedUserName}=args.input;
+            let updatedUser;
+            UserList.forEach((user) => {
+                if(Number(user.id) == Number(id))
+                {
+                    user.username=updatedUserName;
+                    updatedUser=user;
+                }
+            })
+            return updatedUser;
+        },
+        deleteUser:(parent,args) => {
+           const id=args.id;
+           _.remove(UserList,(user)=> user.id === Number(id) );
+           return null;
+        }
+    },
+    User: {
+        favoriteMovies: () => {
+            return _.filter(MovieList, (movie) =>
+                movie.yearOfPublication > 2000 && movie.yearOfPublication < 2010
+            );
         }
     }
 }
 
-module.exports = {resolvers};
+module.exports = { resolvers };
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -1,5 +1,6 @@
 const { gql } = require("apollo-server");
 
+
 const typeDefs = gql`
 
 type Query {
@@ -9,12 +10,34 @@ movies: [Movie!]!
 movie(name: String!): Movie
 }
 
-type User {
+#mutate data (similar to post, put, delete)
+type Mutation {
+createUser( input: createUserInput! ): User
+updateUserName ( input: updateUserNameInput! ): User
+deleteUser (id:ID!): User
+}
+
+input updateUserNameInput {
+id:ID!
+updatedUserName:String!
+}
+
+input createUserInput {
  name: String!
  username: String!
+ age: Int = 18
+ nationality: Nationality
+}
+
+type User {
+ id:ID!,
+ name: String!
+ username: String
  age: Int!
  nationality: Nationality!
  friends: [User]
+ favoriteMovies: [Movie]
+
 }
 
 type Movie {

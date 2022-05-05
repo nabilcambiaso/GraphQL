0ec35a3651c64fd55f1b83cadcd9e8a3c2832c6c
Said Gouzal (Eswaad) <gouzal.said3@gmail.com>
2022-05-05 09:37:28 +0000

    committing...

--- db/seeds/account.js
+++ db/seeds/account.js
@@ -6,8 +6,8 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('account').del()
   await knex('account').insert([
-    { name: 'Account 1', balance: 200, initial_balance: 100, note: "Account 1 test" },
-    { name: 'Account 2', balance: 350, initial_balance: 250, note: "Account 2 test" },
-    { name: 'Account 3', balance: 20, initial_balance: 0, note: "Account 3 test" },
+    { name: 'Account 1', balance: 200, note: "Account 1 test" },
+    { name: 'Account 2', balance: 350, note: "Account 2 test" },
+    { name: 'Account 3', balance: 20, note: "Account 3 test" },
   ]);
 };
